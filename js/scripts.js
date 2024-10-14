55c222223606a5e3d319a29a65790cec143e399d
gabrielgroto <gabriel.groto@redeunifique.com.br>
2024-10-14 10:33:31 -0300

    JS
    Atualizando a página e o  JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-const cepButton = document.querySelector("btnPesuqisar");
+const containerForm = document.querySelector(".contForm");
+const inputCep = document.querySelector("#cep");
+const inputRua = document.querySelector("#rua");
+const inputCidade = document.querySelector("#cidade");
+const inputEstado = document.querySelector("#estado");
+const inputRegiao = document.querySelector("#regiao");
+const inputUf = document.querySelector("#uf");
+const fecharMsg = document.querySelector("#fechar-mensagem");
+
+inputCep.addEventListener("keypress", (evento) => {
+  const accept = /[0-9]/;
+  const keyPress = String.fromCharCode(evento.keyCode);
+  if (!accept.test(keyPress)) {
+    evento.preventDefault();
+    alert("Apenas números de 0-9 são permitidos!");
+    return
+  }
+})
+
+inputCep.addEventListener("keyUp", (evento) => {
+  const valorDoInput = evento.target.value;
+  if(valorDoInput.length === 8) {
+    apiFunction(inputValue);
+    }
+})
+
+const apiFunction = async (cep) => {
+
+}
+
+const mostrarLoader = () => {
+  const background = document.querySelector("#background-info");
+  const loader = document.querySelector("#loader");
+}
